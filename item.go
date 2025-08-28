9de6f1d1a6d9be879a232f48351fcdbebe80090a
NadaSerag-github <nadaserag2006@gmail.com>
2025-08-28 21:45:59 +0300

    added item types

--- item.go
+++ item.go
@@ -3,3 +3,13 @@ package main
 type item struct {
 	name string
 }
+
+type Weapon struct {
+	item        // using composition here
+	damageBonus int
+}
+
+type Armor struct {
+	item
+	defenseBonus int
+}
