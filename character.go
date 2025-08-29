3889a4ef85891d6b2ca2cd9cf67f1adaa06d9eee
NadaSerag-github <nadaserag2006@gmail.com>
2025-08-29 16:48:11 +0300

    Added Warrior, Mage and Archer characters

--- character.go
+++ character.go
@@ -1,6 +1,6 @@
 package main
 
-type character struct {
+type Character struct {
 	name     string
 	health   int
 	strength int
@@ -10,8 +10,8 @@ type character struct {
 
 //creating characters
 
-func NewCharacter(name string) character {
-	newCharacter := character{
+func NewCharacter(name string) Character {
+	newCharacter := Character{
 		name:     name,
 		health:   0,
 		strength: 0,
@@ -20,4 +20,56 @@ func NewCharacter(name string) character {
 	}
 
 	return newCharacter
+}
+
+// Warrior`: High `strength` and `defense`, low `magic`
+// `Mage`: High `magic`, low `strength`
+
+type Warrior struct {
+	Character
+}
+
+func NewWarrior(name string) Warrior {
+	return Warrior{
+		Character: Character{
+			name:     name,
+			health:   120,
+			strength: 100,
+			defense:  100,
+			magic:    10,
+		},
+	}
+}
+
+type Mage struct {
+	Character
+}
+
+func NewWMage(name string) Mage {
+	return Mage{
+		Character: Character{
+			name:     name,
+			health:   120,
+			strength: 20,
+			defense:  80,
+			magic:    100,
+		},
+	}
+}
+
+type Archer struct {
+	Character
+}
+
+// `Archer`: Moderate `strength` and `health`, low `magic`
+func NewArcher(name string) Archer {
+	return Archer{
+		Character: Character{
+			name:     name,
+			health:   90,
+			strength: 90,
+			defense:  70,
+			magic:    10,
+		},
+	}
 }
